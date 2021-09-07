acf5b8408ea15f7772f0ca10a327d6401000fbfd
Xenia Sokol <sokol.xenia@gmail.com>
2021-09-06 21:06:45 -0400

    mapping polyline

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,16 +3,18 @@ console.log("working");
 
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+let map = L.map('mapid').setView([37.6213, -122.3790], 4);
 
 // Coordinates for each point to be used in the line.
 let line = [
   [33.9416, -118.4085],
-  [37.6213, -122.3790]
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
 ];
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -22,6 +24,10 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
-  color: "red"
+  color: "blue",
+  dashArray: "10",
+  weight:4,
+  opacity:0.5,
+  lineCap: 'round',
 }).addTo(map);
 
