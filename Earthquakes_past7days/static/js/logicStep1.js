eabd39f205e228c9000e51efd91e0b6053894628
Xenia Sokol <sokol.xenia@gmail.com>
2021-09-10 22:11:56 -0400

    13.6.1 completed

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,32 +18,35 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Satellite_Streets: satelliteStreets,
+  "Streets": streets,
+  "Satellite": satelliteStreets,
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let torontoHoods = 'https://raw.githubusercontent.com/xenia-e/Mapping_Earthquakes/main/torontoNeighborhoods.json';
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-    coloe: 'blue',
-    weight:1,
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME +'</h3>')
-    }
-  }).addTo(map);
+  L.geoJson(data).addTo(map);
 });
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data, {
+//     coloe: 'blue',
+//     weight:1,
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME +'</h3>')
+//     }
+//   }).addTo(map);
+// });
